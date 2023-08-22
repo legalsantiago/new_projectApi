85eee8c8e48ca2aa86b0f7d1de40a7653dd38721
legalsantiago <smora430@gmail.com>
2023-08-22 11:22:03 -0400

    nuevos archivos

--- drf/settings/production.py
+++ drf/settings/production.py
@@ -9,17 +9,17 @@ ALLOWED_HOSTS = []
 
 import os
 
-if 'RDS_HOSTNAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['apiTest'],
-            'USER': os.environ['triunfo'],
-            'PASSWORD': os.environ['triunfo2023'],
-            'HOST': os.environ['databaseaws1.ctpv9egj42p9.sa-east-1.rds.amazonaws.com'],
-            'PORT': os.environ['5432'],
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['apiTest'],
+        'USER': os.environ['triunfo'],
+        'PASSWORD': os.environ['triunfo2023'],
+        'HOST': os.environ['databaseaws1.ctpv9egj42p9.sa-east-1.rds.amazonaws.com'],
+        'PORT': os.environ['5432'],
     }
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- drf/wsgi.py
+++ drf/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf.settings.production')
 
 application = get_wsgi_application()
